f51d260d39a210079d7c00ca344de27e23d597ba
Rodrigo A. Grijalba <80277672+RodrigoGrijalba@users.noreply.github.com>
2023-06-12 13:27:30 -0500

    hide api key

--- code/test.py
+++ code/test.py
@@ -6,8 +6,6 @@ from langchain.agents import create_pandas_dataframe_agent
 import pandas as pd
 import os
 
-os.environ["OPENAI_API_KEY"]="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"
-
 df = pd.read_csv("output/table.csv", encoding="latin1")
 chat = ChatOpenAI(temperature=0, model_name = "gpt-4")
 agent = create_pandas_dataframe_agent(chat, df)
