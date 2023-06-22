601f650f0cb067c3f6341ba2f2daee9338ac1fae
RodrigoGrijalba <ragrijalba119@gmail.com>
2023-06-22 16:07:49 -0500

    add compressor

--- code/app.py
+++ code/app.py
@@ -2,15 +2,25 @@ import streamlit as st
 from dotenv import load_dotenv
 from langchain.vectorstores import Chroma
 from langchain.embeddings import OpenAIEmbeddings
+from langchain.llms import OpenAI
+from langchain.retrievers import ContextualCompressionRetriever
+from langchain.retrievers.document_compressors import LLMChainExtractor
 from prompts import system_prompt, human_template
 from utils import ensure_fit_tokens, get_page_contents
 from render import bot_msg_container_html_template, user_msg_container_html_template
 import openai
 
 load_dotenv()
+
 embedding = OpenAIEmbeddings()
 vec_db = Chroma(persist_directory="output/test_db", embedding_function=embedding)
 retriever = vec_db.as_retriever(search_type="mmr")
+llm = OpenAI()
+compressor = LLMChainExtractor.from_llm(llm)
+compression_retriever = ContextualCompressionRetriever(
+    base_compressor=compressor, 
+    base_retriever=retriever
+)
 
 def construct_messages(history):
     messages = [{"role": "system", "content": system_prompt}]
@@ -23,7 +33,7 @@ def construct_messages(history):
     return messages
 
 def query_handler(query):
-    relevant_docs = retriever.get_relevant_documents(query)
+    relevant_docs = compression_retriever.get_relevant_documents(query)
     context, metadata = get_page_contents(relevant_docs)
     query_with_context = human_template.format(query=query, context=context, metadata = metadata)
     return {"role": "user", "content": query_with_context}
